a59499f8825a3b653ae0d7835ca60f531db5ed19
Fabio Rosada <r.fabio95@gmail.com>
2024-11-05 23:07:09 +0100

    Added keybinds to manipulate windows within workspace and graphical fixes

--- .config/hypr/scripts/getVolume.sh
+++ .config/hypr/scripts/getVolume.sh
@@ -5,4 +5,6 @@ VOLUME_PERCENT=$(echo "$VOLUME * 100" | bc)
 VOLUME_PERCENT_INT=$(printf "%.0f" "$VOLUME_PERCENT")
 echo $VOLUME_PERCENT_INT >> /tmp/wobpipe
 
+mpv /usr/share/sounds/gnome/default/alerts/click.ogg
+# mpv ~/.config/hypr/sounds/water_drop.opus
 # tail -f /tmp/wobpipe | wob

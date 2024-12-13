bfc265b2537ea2083f78e011454b77dbe23fca4f
Fabio Rosada <r.fabio95@gmail.com>
2024-12-13 20:45:25 +0100

    added different background for dark theme

--- .config/hypr/scripts/setWaybarDark.sh
+++ .config/hypr/scripts/setWaybarDark.sh
@@ -2,8 +2,11 @@
 
 if [ -f ~/.config/waybar/style-dark.css.disabled ]; then
   mv ~/.config/waybar/style-dark.css.disabled ~/.config/waybar/style-dark.css
+  cp ~/.config/hypr/wallpaper/wallpaper-dark ~/.config/hypr/wallpaper/wallpaper
 else
   mv ~/.config/waybar/style-dark.css ~/.config/waybar/style-dark.css.disabled
+  cp ~/.config/hypr/wallpaper/wallpaper-light ~/.config/hypr/wallpaper/wallpaper
 fi
 
 ~/.config/hypr/scripts/restart-waybar.sh
+~/.config/hypr/scripts/restart-hyprpaper.sh

0acec8da4f71a7f0b3b695af3679f20a756608eb
Fabio Rosada <r.fabio95@gmail.com>
2025-02-06 22:28:24 +0100

    added automatic color picker with pywal - integrated with kitty

--- .config/hypr/scripts/setWaybarDark.sh
+++ .config/hypr/scripts/setWaybarDark.sh
@@ -2,10 +2,10 @@
 
 if [ -f ~/.config/waybar/style-dark.css.disabled ]; then
   mv ~/.config/waybar/style-dark.css.disabled ~/.config/waybar/style-dark.css
-  cp ~/.config/hypr/wallpaper/wallpaper-dark ~/.config/hypr/wallpaper/wallpaper
+  ~/.local/bin/set-background ~/.config/hypr/wallpaper/wallpaper-dark
 else
   mv ~/.config/waybar/style-dark.css ~/.config/waybar/style-dark.css.disabled
-  cp ~/.config/hypr/wallpaper/wallpaper-light ~/.config/hypr/wallpaper/wallpaper
+  ~/.local/bin/set-background ~/.config/hypr/wallpaper/wallpaper-light
 fi
 
 ~/.config/hypr/scripts/restart-waybar.sh

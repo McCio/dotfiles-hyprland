34cb24aa79ca0f4924b3d4769d3edaf6887cca5e
Fabio Rosada <r.fabio95@gmail.com>
2025-02-10 15:50:02 +0100

    added placeholder album cover for players with no cover art

--- .config/hypr/scripts/hyprlockMusic.sh
+++ .config/hypr/scripts/hyprlockMusic.sh
@@ -28,12 +28,11 @@ art_url=$(playerctl metadata mpris:artUrl 2>/dev/null)
 current_album=$(playerctl metadata album 2>/dev/null)
 if [[ $art_url != "No players found" &&  $current_album != $(cat /tmp/playerctl-album.txt 2>/dev/null) ]]; then
   if [[ $art_url == http* ]]; then
-    wget -q -O /tmp/playerctl-cover.jpg "$art_url"
-  elif [[ $art_url == file* ]]; then
-    rm -f /tmp/playerctl-cover.jpg
+    wget -q -O /tmp/playerctl-cover.jpg "$art_url"  # download the album cover
+  else 
+    cp ~/.config/hypr/scripts/music-placeholder.jpg /tmp/playerctl-cover.jpg # put a placeholder image
   fi
   echo "$current_album" > /tmp/playerctl-album.txt
 fi
 
 echo "<span><b><big>$(playerctl metadata title)</big></b></span>"
-

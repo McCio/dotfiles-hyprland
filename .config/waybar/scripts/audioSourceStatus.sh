6ca72ac915482582e7de586f63b088cc7200cad1
Marco Ciotola <848222@stud.unive.it>
2025-02-18 17:04:51 +0100

    feat: add media player info and minor styles

--- .config/waybar/scripts/audioSourceStatus.sh
+++ .config/waybar/scripts/audioSourceStatus.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 volume=$(wpctl get-volume @DEFAULT_SOURCE@ | awk '{print $2*100}')
-node_name=$(wpctl inspect @DEFAULT_SOURCE@ -r | awk -F'=' '/node.nick/ {gsub(/\s*"\s*/,"");print $2}')
-class=$(pactl get-source-mute @DEFAULT_SOURCE@ | awk '{print $2}')
-if [ "$class" == "yes" ]
+node_name=$(wpctl inspect @DEFAULT_SOURCE@ | awk -F'=' '/node.(nick|description)/ {gsub(/\s*"\s*/,"");print $2}' | head -n 1)
+muted=$(pactl get-source-mute @DEFAULT_SOURCE@ | awk '{print $2}')
+if [ "$muted" == "yes" ]
 then
-    class="muted"
-    icon="󰍭"
+  class="muted"
+  icon="󰍭"
 else
-    class="unmuted"
-    icon="󰍬"
+  class="unmuted"
+  icon="󰍬"
 fi
 echo "{\"text\":\"$icon\",\"tooltip\":\"$node_name ${volume}%\",\"percentage\": $volume, \"node_name\": \"$node_name\", \"class\": \"$class\", \"alt\": \"$class\"}"

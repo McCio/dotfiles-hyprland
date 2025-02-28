2d65f741b1cc1f578f514cc9dfd8277137fdda6b
Fabio Rosada <r.fabio95@gmail.com>
2025-02-28 19:08:06 +0100

    [Hyprlock] added network and bluetooth status (+ battery, untested)

---
+++ .config/hypr/scripts/hyprlockNetwork.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+connection_info=$(nmcli -t -f active,ssid,signal dev wifi | grep '^yes')
+connection_name=$(echo "$connection_info" | cut -d: -f2)
+signal_strength=$(echo "$connection_info" | cut -d: -f3)
+
+low="󰤟"
+medium="󰤢"
+high="󰤥"
+full="󰤨"
+
+if [ "$signal_strength" -ge 75 ]; then
+  icon=$full
+elif [ "$signal_strength" -ge 50 ]; then
+  icon=$high
+elif [ "$signal_strength" -ge 25 ]; then
+  icon=$medium
+else
+  icon=$low
+fi
+
+if [ -n "$connection_name" ]; then
+  echo "$icon  $connection_name"
+else
+  echo "󱞐  Offline"
+fi
---
+++ .config/hypr/scripts/hyprlockPower.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+if [ -f /sys/class/power_supply/BAT0/status ]; then
+  battery_status=$(cat /sys/class/power_supply/BAT0/status)
+  if [ "$battery_status" = "Discharging" ]; then
+    battery_status="󰁹"
+  elif [ "$battery_status" = "Charging" ]; then
+    battery_status="󰂄"
+  else
+    battery_status="󰁹"
+  fi
+  battery_percentage=$(cat /sys/class/power_supply/BAT0/capacity)
+  echo "$battery_status $battery_percentage%"
+else
+  battery_status=""
+  exit 1
+fi
